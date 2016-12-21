b31836f513212338c24e2f329f629ec6a019bfde
mthomasson <thomassonmaxime@gmail.com>
2016-12-21 13:03:56 +0000

    /Device ok & clean

--- NodeJS_Server/app/DAO/DAO_Device.js
+++ NodeJS_Server/app/DAO/DAO_Device.js
@@ -1,35 +1,23 @@
 'use strict';
-
+var DAO_Connection = require ("./DAO_Connection.js");
 
 
 
 var list = function(deviceList){
-	var mysql      = require('mysql');
-	var connection = mysql.createConnection({
-		host     : 'localhost',
-		user     : 'root',
-		password : 'root',
-		database : 'ProjetMajeure'
-	});
-
-	connection.connect();
-
-	connection.query('SELECT * from device', function(err, DeviceList) {
-		if (err){
-			console.log('Error while performing Query.  ' + err);
-			return deviceList(err);
-			
-		}
-		else
-		{
+	DAO_Connection.connect(function(conn){
+		conn.query('SELECT * from device', function(err, DeviceList) {
+			if (err){
+				console.log('Error while performing Query.  ' + err);
+				return deviceList(err);
+			}
+			else
+			{
 			//console.log('type of : ', typeof(DeviceList));
 			console.log('The solution is: ', DeviceList);
 			return deviceList(null, DeviceList);
-		}
-		
+			}
+		});
+		conn.end();
 	});
-	connection.end();
-	
-
 }
 module.exports.list = list;
--- NodeJS_Server/app/controllers/device.controller.js
+++ NodeJS_Server/app/controllers/device.controller.js
@@ -7,47 +7,17 @@ var path = require("path");
 var DAO_Device = require("./../DAO/DAO_Device.js");
 
 
-var list = function(callback){
+var list = function(DAO_Device_list){
 	DAO_Device.list(function(err, deviceList){
-	if(err)
-	{
-		return response.statut(500).end;
-    }
-    else{
-    	console.log(deviceList);
-    	return callback(null, deviceList);
-       }
+		if(err)
+		{
+			return DAO_Device_list(err);
+		}
+		else{
+			console.log(deviceList);
+			return DAO_Device_list(null, deviceList);
+		}
 	});
-
-
-
-	// var mysql      = require('mysql');
-	// var connection = mysql.createConnection({
-	// 	host     : 'localhost',
-	// 	user     : 'root',
-	// 	password : 'root',
-	// 	database : 'ProjetMajeure'
-	// });
-
-	// connection.connect();
-
-	// connection.query('SELECT * from device', function(err, deviceList) {
-	// 	if (err){
-	// 		console.log('Error while performing Query.  ' + err);
-	// 		return callback(err);
-			
-	// 	}
-	// 	else
-	// 	{
-	// 		//console.log('type of : ', typeof(DeviceList));
-	// 		//console.log('The solution is: ', DeviceList);
-	// 		return callback(null, deviceList);
-	// 	}
-		
-	// });
-	// connection.end();
-//Appel DAO -> Liste JSON
-//module.exports.list = list;
 
 }
 module.exports.list = list;
