5178383fc7d376a792048b8711d905256c3384e6
mthomasson <thomassonmaxime@gmail.com>
2017-01-10 15:08:35 +0000

    twitter ok, with loading anim and bug correc on flow changing

--- NodeJS_Server/public/manager/js/controllers/eventControlleur.js
+++ NodeJS_Server/public/manager/js/controllers/eventControlleur.js
@@ -1,11 +1,16 @@
 angular.module('managerApp').controller('eventCtrl',eventCrtFnt);
 
-eventCrtFnt.$inject=['$scope','$log','$window','$sce','$interval','factory','comm', 'twitter', '$mdDialog'];
+eventCrtFnt.$inject=['$scope','$log','$window','$sce','$interval', '$mdDialog','factory','comm', 'twitter'];
 
-function eventCrtFnt($scope, $log, $window, $sce, $interval, factory, comm, twitter,$mdDialog){
+function eventCrtFnt($scope, $log, $window, $sce, $interval, $mdDialog, factory, comm, twitter){
 
     $scope.deviceMap={};
     $scope.deviceMap.payload="";
+
+    var isTwitterAccountSet = false;
+    var inter = null;
+    var tweetsList = [];
+    $scope.LoadingAnim = true;
 
     var id_manager = 1; // TODO 1 is default manager id, get real manager id
 
@@ -42,51 +47,69 @@ function eventCrtFnt($scope, $log, $window, $sce, $interval, factory, comm, twit
         function(errorPayload) {
           $log.error('failure loading template', errorPayload);
         });
-    }
-    
+    };
+
+
+
+
     $scope.addNewTweet = function() {
+        if (isTwitterAccountSet == false) {
+            isTwitterAccountSet = true;
 
-        // Appending dialog to document.body to cover sidenav in docs app
-        var confirm = $mdDialog.prompt()
-            .textContent('Please enter the name of the twitter account from which you want to load tweets')
-            .placeholder('Twitter Account name')
-            .ariaLabel('accountName')
-            .initialValue("Your brand's Name")
-            //.targetEvent(ev)
-            .ok('Add Tweets!')
-            .cancel('cancel')
-        // You can specify either sting with query selector
-            .openFrom('left')
-        // or an element
-            .closeTo(angular.element(document.querySelector('#right')));
+            // Appending dialog to document.body to cover sidenav in docs app
+            var confirm = $mdDialog.prompt()
+                .textContent('Please enter the name of the twitter account from which you want to load tweets')
+                .placeholder('Twitter Account name')
+                .initialValue("Your brand's Name")
+                //.targetEvent(ev)
+                .ok('Add Tweets!')
+                .cancel('cancel')
+                // You can specify either sting with query selector
+                .openFrom('left')
+                // or an element
+                .closeTo(angular.element(document.querySelector('#right')));
 
-        $mdDialog.show(confirm).then(function(result) {
-            //$scope.status = 'You decided to name your dog ' + result + '.';
-            var available_tweets=twitter.loadTweets(result, 10);
-            available_tweets.then(
-                function(payload) {
-                    var i = 0, len = payload.length;
-                    var updateTweet = function(){
-                        if (i>=len-1){
-                            i=0;
-                        }
-                        i+=1;
-                        var item = {"html": payload[i].html};
-                        $scope.EmbedTweet = $sce.trustAsHtml(item.html);
+            $mdDialog.show(confirm).then(function (result) {
 
-                    };
-                    $interval(updateTweet, 5000);
+                // TODO Here send result to db to add twitter account to screen DB
+                $scope.LoadingAnim = false;
+                var available_tweets = twitter.loadTweets(result, 10);
+                available_tweets.then(
+                    function (payload) {
+                        tweetsList = payload;
+                        var i = 0, len = tweetsList.length;
+                        var updateTweet = function () {
+                            if (i >= len - 1) {
+                                i = 0;
+                            }
+                            i += 1;
+                            var item = {"html": tweetsList[i].html};
+                            $scope.EmbedTweet = $sce.trustAsHtml(item.html);
+                            $scope.LoadingAnim = true;
+
+                        };
+                        inter = $interval(updateTweet, 5000);
+                    });
+            }, function () {
+                $scope.status = 'You didn\'t add a tweet';
+            });
+        }
+        else{
+                var confirm = $mdDialog.confirm()
+                    .textContent('Would you like to delete your twitter account?')
+                    .ok('Please do it!')
+                    .cancel('Sounds like a scam');
+
+                $mdDialog.show(confirm).then(function() {
+                    $interval.cancel(inter);
+                    tweetsList = [];
+                    isTwitterAccountSet = false;
+                }, function() {
+                    $scope.status = 'You decided to keep your debt.';
                 });
-        }, function() {
-            $scope.status = 'You didn\'t name your dog.';
-        });
+            };
 
-
-
-
-
-
-      };
+        };
 
     
   };
