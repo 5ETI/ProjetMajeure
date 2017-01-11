400ac3713009028ac5f73e210fd1c2c17a60bee4
bertrandp <bertrand.pestre@hotmail.fr>
2017-01-11 13:58:14 +0100

    load content from db with index

--- NodeJS_Server/public/manager/js/controllers/eventControlleur.js
+++ NodeJS_Server/public/manager/js/controllers/eventControlleur.js
@@ -8,7 +8,9 @@ function eventCrtFnt($scope, $log, $window, $sce, $interval, $mdDialog, factory,
   $scope.deviceMap={};
   $scope.deviceMap.payload="";
 
-  var isTwitterAccountSet = false;
+  $scope.id_screen = 0;
+  $scope.screen = {};
+
   var inter = null;
   var tweetsList = [];
   $scope.LoadingAnim = true;
@@ -25,26 +27,13 @@ function eventCrtFnt($scope, $log, $window, $sce, $interval, $mdDialog, factory,
       $log.error('failure loading devices', errorPayload);
     });
 
-    /*$scope.selectDevice = function(payload) { 
-      $scope.deviceMap.payload= payload;             
-      for(key in $scope.deviceMap.payload){
-        $scope.currentDevice = $scope.deviceMap.payload[key];
-      }  
-    },
-    function(errorPayload) {
-      $log.error('failure loading movie', errorPayload);
-    }*/
 
-    $scope.id_screen = 0;
-    $scope.screen = {};
-    //$scope.contents = [];
+  $scope.selectCurrentDevice=function(deviceId){
+    $scope.currentDevice=$scope.deviceMap.array[deviceId-1];
 
-    $scope.selectCurrentDevice=function(deviceId){
-      $scope.currentDevice=$scope.deviceMap.array[deviceId-1];
-
-      var screen = comm.getScreen(id_manager, deviceId);
-      screen.then(
-        function(payload){
+    var screen = comm.getScreen(id_manager, deviceId);
+    screen.then(
+      function(payload){
           //log.info('screen ', payload);
           $scope.currentDevice.template = payload[0].template;
           $scope.id_screen = payload[0].id;
@@ -57,103 +46,82 @@ function eventCrtFnt($scope, $log, $window, $sce, $interval, $mdDialog, factory,
           $log.error('failure loading screen', errorPayload);
         });
 
-      var loadContent = function(){
-        if(!$scope.screen.empty){
-          var contents=comm.loadContent($scope.id_screen);
-          contents.then(
-            function(payload) { 
-              $scope.screen.contents = payload;
-            },
-            function(errorPayload) {
-              $log.error('failure loading content', errorPayload);
-            });
+    var loadContent = function(){
+      if(!$scope.screen.empty){
+        var contents=comm.loadContent($scope.id_screen);
+        contents.then(
+          function(payload) { 
+
+            $scope.screen.contents = [];
+            $log.info("contents lentgh " + $scope.screen.contents.length);
+
+            for(var i=0; i< payload.length ; i++){
+              var j = payload[i].index;
+              $scope.screen.contents[j] = payload[i];
+          }; 
+
+            for(var i=0; i< $scope.screen.contents.length ; i++){
+              if($scope.screen.contents[i].type == 4){ // twitter
+              loadTweets($scope.screen.contents[i].param1);
+          } 
         }
+      },
+      function(errorPayload) {
+        $log.error('failure loading content', errorPayload);
+      });
       }
     }
-    
-    
-    $scope.addNewTweet = function() {
+  }
 
-        // Appending dialog to document.body to cover sidenav in docs app
-        var confirm = $mdDialog.prompt()
-        .textContent('Please enter the name of the twitter account from which you want to load tweets')
-        .placeholder('Twitter Account name')
-        .ariaLabel('accountName')
-        .initialValue("Your brand's Name")
-            //.targetEvent(ev)
-            .ok('Add Tweets!')
-            .cancel('cancel')
-        // You can specify either sting with query selector
-        .openFrom('left')
-        // or an element
-        .closeTo(angular.element(document.querySelector('#right')));
 
-        $mdDialog.show(confirm).then(function(result) {
-            //$scope.status = 'You decided to name your dog ' + result + '.';
-            var available_tweets=twitter.loadTweets(result, 10);
-            available_tweets.then(
-              function(payload) {
-                var i = 0, len = payload.length;
-                var updateTweet = function(){
-                  if (i>=len-1){
-                    i=0;
-                  }
-                  i+=1;
-                  var item = {"html": payload[i].html};
-                  $scope.EmbedTweet = $sce.trustAsHtml(item.html);
+  $scope.remove = function(id_content){
+    var confirm = $mdDialog.confirm()
+    .textContent('Confirm delete this content')
+    .ok('Delete')
+    .cancel('Cancel');
 
-                };
-                $interval(updateTweet, 5000);
-              });
-          }, function() {
-            $scope.status = 'You didn\'t name your dog.';
-          });
+    $mdDialog.show(confirm).then(function(result) {
 
-      };
+      if($scope.screen.contents[id_content].type == 4 ){
+        $interval.cancel(inter);
+        tweetsList = [];
+        $scope.EmbedTweet = "";
+      }
+      $scope.screen.contents[id_content].type = 0;
+    }, function() {
 
-      $scope.remove = function(id_content){
-        var confirm = $mdDialog.confirm()
-        .textContent('Confirm delete this picture')
-        .ok('Delete')
-        .cancel('Cancel');
+    });
+  };
 
-        $mdDialog.show(confirm).then(function(result) {
-            //$scope.status = 'You decided to name your dog ' + result + '.';
-            $scope.screen.contents[id_content].type = 0;
-          }, function() {
+  $scope.edit = function(id_content){
+    var confirm = $mdDialog.prompt()
+    .textContent('Specify the picture url')
+    .placeholder('url')
+    .ariaLabel('url')
+    .ok('Add Image')
+    .cancel('cancel');
 
-          });
-      };
-
-      $scope.edit = function(id_content){
-        var confirm = $mdDialog.prompt()
-        .textContent('Specify the picture url')
-        .placeholder('url')
-        .ariaLabel('url')
-        .ok('Add Image')
-        .cancel('cancel');
-
-        $mdDialog.show(confirm).then(function(result) {
+    $mdDialog.show(confirm).then(function(result) {
             //$scope.status = 'You decided to name your dog ' + result + '.';
             $scope.screen.contents[id_content].type = 1;
             $scope.screen.contents[id_content].param1 = result;
           }, function() {
 
           });
-      };
+  };
 
-      $scope.upload = function (id_content){
-        $scope.screen.contents[id_content].type = 2;
-      };
+  $scope.upload = function (id_content){
+    $scope.screen.contents[id_content].type = 2;
+  };
 
-      $scope.save = function(){
+  $scope.save = function(){
 
-        $log.info("$scope.screen.id " + $scope.id_screen);
-        $log.info(" $scope.screen.contents[0] " +  $scope.screen.contents[0]);
+    $log.info("$scope.screen.id " + $scope.id_screen);
+    $log.info(" $scope.screen.contents[0] " +  $scope.screen.contents[0]);
 
-        var deleteContent = comm.deleteContent($scope.id_screen);
-        deleteContent.then(
-          function(payload){
+    var deleteContent = comm.deleteContent($scope.id_screen);
+    deleteContent.then(
+      function(payload){
           //log.info('screen ', payload);
           
           var save = comm.saveScreen($scope.id_screen, $scope.screen.contents);
@@ -170,17 +138,43 @@ function eventCrtFnt($scope, $log, $window, $sce, $interval, $mdDialog, factory,
           $log.error('failure saving screen', errorPayload);
         });
 
-      };
+  };
 
-      $scope.addNewTweet = function() {
-        if (isTwitterAccountSet == false) {
-          isTwitterAccountSet = true;
+  var loadTweets = function(twitter_account){
+    $scope.LoadingAnim = false;
 
+    $interval.cancel(inter);
+    tweetsList = [];
+    $scope.EmbedTweet = "";
+
+    var available_tweets = twitter.loadTweets(twitter_account, 10);
+    available_tweets.then(
+      function (payload) {
+        tweetsList = payload;
+        var i = 0, len = tweetsList.length;
+        var updateTweet = function () {
+          if (i >= len - 1) {
+            i = 0;
+          }
+          i += 1;
+          var item = {"html": tweetsList[i].html};
+          $scope.EmbedTweet = $sce.trustAsHtml(item.html);
+          $scope.LoadingAnim = true;
+
+        };
+        inter = $interval(updateTweet, 5000);
+      });
+  };
+
+  $scope.addNewTweet = function(id_content) {
+
+   $interval.cancel(inter);
+   tweetsList = [];
+   $scope.EmbedTweet = "";
             // Appending dialog to document.body to cover sidenav in docs app
             var confirm = $mdDialog.prompt()
             .textContent('Please enter the name of the twitter account from which you want to load tweets')
             .placeholder('Twitter Account name')
-            .initialValue("Your brand's Name")
                 //.targetEvent(ev)
                 .ok('Add Tweets!')
                 .cancel('cancel')
@@ -191,44 +185,15 @@ function eventCrtFnt($scope, $log, $window, $sce, $interval, $mdDialog, factory,
 
                 $mdDialog.show(confirm).then(function (result) {
 
-                // TODO Here send result to db to add twitter account to screen DB
-                $scope.LoadingAnim = false;
-                var available_tweets = twitter.loadTweets(result, 10);
-                available_tweets.then(
-                  function (payload) {
-                    tweetsList = payload;
-                    var i = 0, len = tweetsList.length;
-                    var updateTweet = function () {
-                      if (i >= len - 1) {
-                        i = 0;
-                      }
-                      i += 1;
-                      var item = {"html": tweetsList[i].html};
-                      $scope.EmbedTweet = $sce.trustAsHtml(item.html);
-                      $scope.LoadingAnim = true;
-
-                    };
-                    inter = $interval(updateTweet, 5000);
-                  });
-              }, function () {
-                $scope.status = 'You didn\'t add a tweet';
-              });
-              }
-              else{
-                var confirm = $mdDialog.confirm()
-                .textContent('Would you like to delete your twitter account?')
-                .ok('Please do it!')
-                .cancel('Sounds like a scam');
-
-                $mdDialog.show(confirm).then(function() {
-                  $interval.cancel(inter);
-                  tweetsList = [];
-                  isTwitterAccountSet = false;
-                }, function() {
-                  $scope.status = 'You decided to keep your debt.';
+                  $log.info("addNewTweet " + result);
+                  $scope.screen.contents[id_content].type = 4;
+                  $scope.screen.contents[id_content].param1 = result;
+                  loadTweets(result);
+                }, function () {
+                  $scope.status = 'You didn\'t add a tweet';
                 });
+
               };
 
             };
 
-          };
